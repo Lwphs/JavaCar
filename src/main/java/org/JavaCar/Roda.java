f99fc0d0d3116271692cfc3b2e608d8d8400ecdf
HectorGarciaCarlota <arnau.garcia@gracia.lasalle.cat>
2025-03-14 15:46:04 +0100

    Afegir atributs privats

--- src/main/java/org/JavaCar/Roda.java
+++ src/main/java/org/JavaCar/Roda.java
@@ -1,8 +1,8 @@
 package org.JavaCar;
 
 public class Roda {
-    String marca;
-    double diametre;
+    private String marca;
+    private double diametre;
 
     public Roda(String marca, double diametre) {
         this.marca = marca;
