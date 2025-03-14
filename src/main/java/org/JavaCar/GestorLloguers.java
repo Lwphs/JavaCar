d69f7e238866d842e4057a10c131c553d306a0c0
HectorGarciaCarlota <arnau.garcia@gracia.lasalle.cat>
2025-03-14 14:46:42 +0100

    Arrgelar GestorVehicles

--- src/main/java/org/JavaCar/GestorLloguers.java
+++ src/main/java/org/JavaCar/GestorLloguers.java
@@ -1,11 +1,16 @@
 package org.JavaCar;
+import java.util.List;
 
 public class GestorLloguers {
-    public static double calcularIngressosTotals(List<Vehicle>, int dies){
-
+    public static double calcularIngressosTotals(List<Vehicle> vehicles, int dies){
+        double total = 0;
+        for (Vehicle vehicle : vehicles){
+            total =  * dies;
+        }
     }
 
-    public static List<Vehicle> filtrarPerPreu(List<Vehicle>, double preuMax) {
+    public static List<Vehicle> filtrarPerPreu(List<Vehicle> vehicles, double preuMax) {
         //si cilindrada supera 500cc se li afegeix 5 euros per dia?
+        return vehicles;
     }
 }
