12fc662d7f155e378a99beccff80410179786507
HectorGarciaCarlota <arnau.garcia@gracia.lasalle.cat>
2025-03-18 14:47:07 +0100

    Contingut classe Usuari

---
+++ src/main/java/org/JavaCar/Usuari.java
@@ -0,0 +1,15 @@
+package org.JavaCar;
+
+public class Usuari {
+    private String nom;
+    private String cognom;
+    private String telefon;
+    private String email;
+
+    public Usuari(String nom, String cognom, String telefon, String email) {
+        this.nom = nom;
+        this.cognom = cognom;
+        this.telefon = telefon;
+        this.email = email;
+    }
+}
