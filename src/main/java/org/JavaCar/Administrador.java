e23d78163679482f00fda6d30ad4f7963ddf89cc
HectorGarciaCarlota <arnau.garcia@gracia.lasalle.cat>
2025-03-18 14:49:40 +0100

    Creació classes Usuari i Administrador

---
+++ src/main/java/org/JavaCar/Administrador.java
@@ -0,0 +1,8 @@
+package org.JavaCar;
+
+public class Administrador extends Usuari {
+
+    public Administrador(String nom, String cognom, String telefon, String email) {
+        super(nom, cognom, telefon, email);
+    }
+}
---
+++ src/main/java/org/JavaCar/Client.java
@@ -0,0 +1,7 @@
+package org.JavaCar;
+
+public class Client extends Usuari {
+    public Client(String nom, String cognom, String telefon, String email) {
+        super(nom, cognom, telefon, email);
+    }
+}
